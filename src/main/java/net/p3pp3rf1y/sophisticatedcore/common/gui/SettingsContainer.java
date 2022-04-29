c53b49ba7736bef40f7652e4f2eba7fcb085cb26
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-04-29 23:16:04 +0200

    fix: 🐛 Fixed to properly sync slots in settings screen

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/SettingsContainer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/SettingsContainer.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedcore.common.gui;
 
+import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
@@ -9,6 +10,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.ContainerListener;
+import net.minecraft.world.inventory.ContainerSynchronizer;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
@@ -34,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public abstract class SettingsContainer<S extends IStorageWrapper> extends AbstractContainerMenu implements ISyncedContainer {
 	private static final Map<String, ISettingsContainerFactory<?, ?>> SETTINGS_CONTAINER_FACTORIES;
@@ -53,9 +56,11 @@ public abstract class SettingsContainer<S extends IStorageWrapper> extends Abstr
 
 	private final StorageBackgroundProperties storageBackgroundProperties;
 	private final List<Slot> storageInventorySlots = new ArrayList<>();
-	public final NonNullList<ItemStack> ghostItemStacks = NonNullList.create();
+	public final NonNullList<ItemStack> lastGhostSlots = NonNullList.create();
+	public final NonNullList<ItemStack> remoteGhostSlots = NonNullList.create();
 	private final Map<String, SettingsContainerBase<?>> settingsContainers = new LinkedHashMap<>();
 	public final List<Slot> ghostSlots = new ArrayList<>();
+
 	protected SettingsContainer(MenuType<?> menuType, int windowId, Player player, S storageWrapper) {
 		super(menuType, windowId);
 		this.player = player;
@@ -97,39 +102,82 @@ public abstract class SettingsContainer<S extends IStorageWrapper> extends Abstr
 	protected Slot addSlot(Slot slot) {
 		slot.index = ghostSlots.size();
 		ghostSlots.add(slot);
-		ghostItemStacks.add(ItemStack.EMPTY);
+		lastGhostSlots.add(ItemStack.EMPTY);
+		remoteGhostSlots.add(ItemStack.EMPTY);
 		return slot;
 	}
 
 	@Override
 	public void broadcastChanges() {
-		for (int i = 0; i < ghostSlots.size(); ++i) {
-			ItemStack itemstack = ghostSlots.get(i).getItem();
-			ItemStack itemstack1 = ghostItemStacks.get(i);
-			if (!ItemStack.matches(itemstack1, itemstack)) {
-				boolean clientStackChanged = !itemstack1.equals(itemstack, true);
-				ItemStack itemstack2 = itemstack.copy();
-				ghostItemStacks.set(i, itemstack2);
+		for (int slot = 0; slot < ghostSlots.size(); ++slot) {
+			ItemStack itemstack = ghostSlots.get(slot).getItem();
+			Supplier<ItemStack> supplier = Suppliers.memoize(itemstack::copy);
+			triggerSlotListeners(slot, itemstack, supplier);
+			synchronizeSlotToRemote(slot, itemstack, supplier);
+		}
+	}
 
-				if (clientStackChanged) {
-					for (ContainerListener icontainerlistener : containerListeners) {
-						icontainerlistener.slotChanged(this, i, itemstack2);
-					}
+	@Override
+	public void broadcastFullState() {
+		for (int slotIndex = 0; slotIndex < ghostSlots.size(); ++slotIndex) {
+			ItemStack itemstack = ghostSlots.get(slotIndex).getItem();
+			triggerSlotListeners(slotIndex, itemstack, itemstack::copy);
+		}
+
+		sendAllDataToRemote();
+	}
+
+	@SuppressWarnings("java:S2177")
+	private void triggerSlotListeners(int slotIndex, ItemStack slotStack, Supplier<ItemStack> slotStackCopy) {
+		ItemStack itemstack = lastGhostSlots.get(slotIndex);
+		if (!ItemStack.matches(itemstack, slotStack)) {
+			boolean clientStackChanged = !slotStack.equals(itemstack, true);
+			ItemStack itemstack1 = slotStackCopy.get();
+			lastGhostSlots.set(slotIndex, itemstack1);
+
+			if (clientStackChanged) {
+				for (ContainerListener containerlistener : containerListeners) {
+					containerlistener.slotChanged(this, slotIndex, itemstack1);
 				}
 			}
 		}
 
 	}
 
+	@SuppressWarnings("java:S2177")
+	private void synchronizeSlotToRemote(int slotIndex, ItemStack slotStack, Supplier<ItemStack> slotStackCopy) {
+		if (!suppressRemoteUpdates) {
+			ItemStack itemstack = remoteGhostSlots.get(slotIndex);
+			if (!ItemStack.matches(itemstack, slotStack)) {
+				ItemStack stackCopy = slotStackCopy.get();
+				remoteGhostSlots.set(slotIndex, stackCopy);
+				if (synchronizer != null) {
+					synchronizer.sendSlotChange(this, slotIndex, stackCopy);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void sendAllDataToRemote() {
+		for (int slotIndex = 0; slotIndex < ghostSlots.size(); slotIndex++) {
+			remoteGhostSlots.set(slotIndex, ghostSlots.get(slotIndex).getItem().copy());
+		}
+
+		if (synchronizer != null) {
+			synchronizer.sendInitialData(this, remoteSlots, remoteCarried, new int[0]);
+		}
+	}
+
 	public abstract void detectSettingsChangeAndReload();
 
 	@Override
-	public void addSlotListener(ContainerListener listener) {
-		if (listener instanceof ServerPlayer serverPlayer && storageWrapper.getInventoryHandler().getStackSizeMultiplier() > 1) {
+	public void setSynchronizer(ContainerSynchronizer synchronizer) {
+		if (player instanceof ServerPlayer serverPlayer && storageWrapper.getInventoryHandler().getStackSizeMultiplier() > 1) {
 			super.setSynchronizer(new HighStackCountSynchronizer(serverPlayer));
 			return;
 		}
-		super.addSlotListener(listener);
+		super.setSynchronizer(synchronizer);
 	}
 
 	@Override
