bea193c4a228d465c4a32c052851504f6bd691af
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-08-12 14:06:49 +0200

    feat: ✨ Added storage link block that allows linking storages to controller even if they are not directly connected to it. Also added storage tool that's used for the linking to controller. And added debug tool (no recipe as it's meant for debugging issues with controller only) that for now shows a bit of information about connected blocks visually, possibly more additions will happen in future as needed.

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/controller/ILinkable.java
@@ -0,0 +1,44 @@
+package net.p3pp3rf1y.sophisticatedcore.controller;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface ILinkable extends IControllerBoundable {
+	default void linkToController(BlockPos controllerPos) {
+		if (!canBeLinked() || isLinked()) {
+			return;
+		}
+
+		Level level = getStorageBlockLevel();
+
+		if (!level.isClientSide() && WorldHelper.getLoadedBlockEntity(level, controllerPos, ControllerBlockEntityBase.class).isPresent()) {
+			setControllerPos(controllerPos);
+			runOnController(level, controller -> controller.addLinkedBlock(getStorageBlockPos()));
+		}
+	}
+
+	default void unlinkFromController() {
+		Level level = getStorageBlockLevel();
+
+		if (!level.isClientSide()) {
+			runOnController(level, controller -> controller.removeLinkedBlock(getStorageBlockPos()));
+			removeControllerPos();
+			setNotLinked();
+		}
+	}
+	default void setNotLinked() {
+		//noop by default
+	}
+	Set<BlockPos> getConnectablePositions();
+	boolean connectLinkedSelf();
+	void setControllerPos(BlockPos controllerPos);
+	Optional<BlockPos> getControllerPos();
+	boolean isLinked();
+	default boolean canBeLinked() {
+		return true;
+	}
+}
