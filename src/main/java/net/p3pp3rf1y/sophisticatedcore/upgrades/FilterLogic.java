91ff0a652021ba79fb78745f59850f1fa91c5f28
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-12-14 13:34:09 +0100

    fix: 🐛 Fixed upgrading refill upgrade to advanced

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/FilterLogic.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/FilterLogic.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fml.util.thread.SidedThreadGroups;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.FilterItemStackHandler;
 import net.p3pp3rf1y.sophisticatedcore.util.InventoryHelper;
@@ -98,7 +99,9 @@ public class FilterLogic extends FilterLogicBase {
 
 	public class ObservableFilterItemStackHandler extends FilterItemStackHandler {
 		private IntConsumer onSlotChange = s -> {};
-		public ObservableFilterItemStackHandler() {super(FilterLogic.this.filterSlotCount);}
+		public ObservableFilterItemStackHandler() {
+			super(filterSlotCount);
+		}
 
 		@Override
 		protected void onContentsChanged(int slot) {
@@ -114,7 +117,13 @@ public class FilterLogic extends FilterLogicBase {
 
 		@Override
 		public void deserializeNBT(CompoundTag nbt) {
-			setSize(NBTHelper.getInt(nbt, "Size").orElse(filterSlotCount));
+			int size = NBTHelper.getInt(nbt, "Size").orElse(filterSlotCount);
+			if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER && filterSlotCount > size) {
+				NBTHelper.putInt(nbt, "Size", filterSlotCount);
+				save();
+				size = filterSlotCount;
+			}
+			setSize(size);
 			ListTag tagList = nbt.getList("Items", Tag.TAG_COMPOUND);
 			for (int i = 0; i < tagList.size(); i++) {
 				CompoundTag itemTags = tagList.getCompound(i);
