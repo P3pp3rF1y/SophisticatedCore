838466106b6cc7d48b0d90993773bc5f0999c7c6
P3pp3rF1y <p3pp3rf1y@gmail.com>
2023-09-16 10:32:29 +0200

    fix: 🐛 Fix item borders compat not to crash on dedicated server

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/itemborders/ItemBordersCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/itemborders/ItemBordersCompat.java
@@ -3,13 +3,15 @@ package net.p3pp3rf1y.sophisticatedcore.compat.itemborders;
 import com.anthonyhilyard.itemborders.ItemBorders;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.inventory.Slot;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 
 public class ItemBordersCompat implements ICompat {
 	@Override
 	public void setup() {
-		StorageScreenBase.setSlotDecorationRenderer(ItemBordersCompat::renderItemBorder);
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> StorageScreenBase.setSlotDecorationRenderer(ItemBordersCompat::renderItemBorder));
 	}
 
 	private static void renderItemBorder(GuiGraphics guiGraphics, Slot slot) {
