e8af0a89c290a0d82dd2a33baf36566e4f6a072e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2024-08-19 15:56:12 +0200

    feat: 🐛 Add experience bucket

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModFluids.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModFluids.java
@@ -1,8 +1,13 @@
 package net.p3pp3rf1y.sophisticatedcore.init;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.bus.api.IEventBus;
@@ -19,7 +24,7 @@ public class ModFluids {
 	}
 
 	private static BaseFlowingFluid.Properties fluidProperties() {
-		return new BaseFlowingFluid.Properties(XP_FLUID_TYPE, XP_STILL, XP_FLOWING);
+		return new BaseFlowingFluid.Properties(XP_FLUID_TYPE, XP_STILL, XP_FLOWING).bucket(XP_BUCKET);
 	}
 
 	public static final ResourceLocation EXPERIENCE_TAG_NAME = ResourceLocation.fromNamespaceAndPath("c", "experience");
@@ -33,8 +38,20 @@ public class ModFluids {
 	public static final Supplier<FlowingFluid> XP_FLOWING = FLUIDS.register("xp_flowing", () -> new BaseFlowingFluid.Flowing(fluidProperties()));
 	public static final Supplier<FluidType> XP_FLUID_TYPE = FLUID_TYPES.register("experience", () -> new FluidType(FluidType.Properties.create().lightLevel(10).density(800).viscosity(1500)));
 
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, SophisticatedCore.MOD_ID);
+	public static final Supplier<Item> XP_BUCKET = ITEMS.register("xp_bucket", () -> new BucketItem(XP_STILL.get(), new Item.Properties().stacksTo(1)));
+
+	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB.location(), SophisticatedCore.MOD_ID);
+	public static final Supplier<CreativeModeTab> CREATIVE_TAB = CREATIVE_MODE_TABS.register("main", () ->
+			CreativeModeTab.builder().icon(() -> new ItemStack(XP_BUCKET.get()))
+					.title(Component.translatable("itemGroup.sophisticatedbackpacks"))
+					.displayItems((featureFlags, output) -> output.accept(new ItemStack(XP_BUCKET.get())))
+					.build());
+
 	public static void registerHandlers(IEventBus modBus) {
 		FLUIDS.register(modBus);
 		FLUID_TYPES.register(modBus);
+		ITEMS.register(modBus);
+		CREATIVE_MODE_TABS.register(modBus);
 	}
 }
