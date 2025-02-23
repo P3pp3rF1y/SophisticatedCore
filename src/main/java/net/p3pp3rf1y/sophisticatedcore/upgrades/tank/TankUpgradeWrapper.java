f99f2bf3daf21b279b1a04c1f213af8b64173d05
Mephodio <40513410+Mephodio@users.noreply.github.com>
2025-02-23 20:25:04 +0300

    Fix draiing / filling containers with fluid that has attached NBT

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/tank/TankUpgradeWrapper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/tank/TankUpgradeWrapper.java
@@ -84,7 +84,7 @@ public class TankUpgradeWrapper extends UpgradeWrapperBase<TankUpgradeWrapper, T
 		boolean tankEmpty = contents.isEmpty();
 		for (int tank = 0; tank < fluidHandler.getTanks(); tank++) {
 			FluidStack fluidInTank = fluidHandler.getFluidInTank(tank);
-			if (!tankEmpty && fluidInTank.getFluid() == contents.getFluid()) {
+			if (!tankEmpty && fluidInTank.isFluidEqual(contents)) {
 				return false;
 			} else if (!fluidInTank.isEmpty()) {
 				return false;
@@ -100,7 +100,7 @@ public class TankUpgradeWrapper extends UpgradeWrapperBase<TankUpgradeWrapper, T
 			int tankCapacity = fluidHandler.getTankCapacity(tank);
 			if (tankEmpty && fluidInTank.getAmount() < tankCapacity) {
 				return false;
-			} else if (contents.getFluid() == fluidInTank.getFluid() && fluidInTank.getAmount() < tankCapacity) {
+			} else if (fluidInTank.isFluidEqual(contents) && fluidInTank.getAmount() < tankCapacity) {
 				return false;
 			}
 		}
@@ -300,7 +300,7 @@ public class TankUpgradeWrapper extends UpgradeWrapperBase<TankUpgradeWrapper, T
 
 	private Boolean isFullAfterFillButUnableToInsertIntoResult(ItemStack containerCopy) {
 		return getFluidHandler(containerCopy).map(copyFluidHandler -> {
-			copyFluidHandler.fill(new FluidStack(contents.getFluid(), Math.min(FluidType.BUCKET_VOLUME, contents.getAmount())), IFluidHandler.FluidAction.EXECUTE);
+			copyFluidHandler.fill(new FluidStack(contents, Math.min(FluidType.BUCKET_VOLUME, contents.getAmount())), IFluidHandler.FluidAction.EXECUTE);
 			int tank = getMatchingTank(copyFluidHandler, contents);
 			if (tank < 0) {
 				return true;
@@ -317,7 +317,7 @@ public class TankUpgradeWrapper extends UpgradeWrapperBase<TankUpgradeWrapper, T
 		if (isValidFluidHandler(fluidHandler, false)) {
 			FluidStack extracted = contents.isEmpty() ?
 					fluidHandler.drain(FluidType.BUCKET_VOLUME, IFluidHandler.FluidAction.SIMULATE) :
-					fluidHandler.drain(new FluidStack(contents.getFluid(), Math.min(FluidType.BUCKET_VOLUME, getTankCapacity() - contents.getAmount())), IFluidHandler.FluidAction.SIMULATE);
+					fluidHandler.drain(new FluidStack(contents, Math.min(FluidType.BUCKET_VOLUME, getTankCapacity() - contents.getAmount())), IFluidHandler.FluidAction.SIMULATE);
 
 			if (extracted.isEmpty()) {
 				return false;
