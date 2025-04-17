83bb75e9e8ceaa233dfcecfa80ea213dcaa54d3e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-04-18 00:20:32 +0200

    refactor: ♻️ Refactor TrashSlot screen to layout registration to happen after mod loading to make sure all mods had a chance to register their screens

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/trashslot/TrashSlotCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/trashslot/TrashSlotCompat.java
@@ -1,12 +1,27 @@
 package net.p3pp3rf1y.sophisticatedcore.compat.trashslot;
 
 import net.blay09.mods.trashslot.api.TrashSlotAPI;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 
 public class TrashSlotCompat implements ICompat {
 	@Override
+	public void init(IEventBus modBus) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			modBus.addListener(this::onLoadComplete);
+		}
+	}
+
+	@Override
 	public void setup() {
-		TrashSlotScreenRegistry.getRegisteredScreens()
-				.forEach(screenClass -> TrashSlotAPI.registerLayout(screenClass, SophisticatedContainerLayout.INSTANCE));
+		//noop
+	}
+
+	private void onLoadComplete(FMLLoadCompleteEvent event) {
+		event.enqueueWork(() ->	TrashSlotScreenRegistry.getRegisteredScreens()
+				.forEach(screenClass -> TrashSlotAPI.registerLayout(screenClass, SophisticatedContainerLayout.INSTANCE)));
 	}
 }
