354f29517677075312483cfab7caf69fd4fbc9b7
Salandora <salandora@gmail.com>
2025-05-12 19:07:59 +0200

    chore: Remove code duplication in ReiSettingsGhostIngredientHandler and ReiStorageGhostIngredientHandler and make it more readable also addressing the AABB::contains comment

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/rei/ReiSettingsGhostIngredientHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/rei/ReiSettingsGhostIngredientHandler.java
@@ -2,14 +2,9 @@ package net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.rei;
 
 import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.client.gui.drag.DraggableStack;
-import me.shedaniel.rei.api.client.gui.drag.DraggableStackVisitor;
-import me.shedaniel.rei.api.client.gui.drag.DraggedAcceptorResult;
-import me.shedaniel.rei.api.client.gui.drag.DraggingContext;
+import me.shedaniel.rei.api.client.gui.drag.*;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.SettingsScreen;
@@ -30,27 +25,15 @@ public class ReiSettingsGhostIngredientHandler<S extends SettingsScreen> impleme
 
 	@Override
 	public DraggedAcceptorResult acceptDraggedStack(DraggingContext<S> context, DraggableStack stack) {
-		Stream<BoundsProvider> bounds = getDraggableAcceptingBounds(context, stack);
-		Point cursor = context.getCurrentPosition();
+        Point cursor = context.getCurrentPosition();
 		if (cursor != null) {
-			int x = cursor.getX();
-			int y = cursor.getY();
-			Optional<BoundsProvider> target = bounds.filter(b -> {
-				AABB box = b.bounds().bounds();
-				double minX = box.minX;
-				double minY = box.minY;
-				double maxX = box.maxX;
-				double maxY = box.maxY;
-				return x >= minX && x <= maxX && y >= minY && y <= maxY && b instanceof GhostTarget;
-			}).findFirst();
+            Optional<ReiGhostTarget> target = getDraggableAcceptingBounds(context, stack)
+					.map(ReiGhostTarget.class::cast)
+					.filter(b -> b.contains(cursor.getX(), cursor.getY()))
+					.findFirst();
 			if (target.isPresent()) {
-				//noinspection unchecked
-				GhostTarget<ItemStack, ? extends SettingsScreen> ghost = (GhostTarget<ItemStack, ? extends SettingsScreen>) target.get();
-				Object held = stack.getStack().getValue();
-				if (held instanceof ItemStack item) {
-					ghost.accept(item);
-					return DraggedAcceptorResult.CONSUMED;
-				}
+				target.get().accept();
+				return DraggedAcceptorResult.CONSUMED;
 			}
 		}
 		return DraggableStackVisitor.super.acceptDraggedStack(context, stack);
@@ -66,7 +49,17 @@ public class ReiSettingsGhostIngredientHandler<S extends SettingsScreen> impleme
 				if (tab instanceof MemorySettingsTab) {
 					screen.getMenu().getStorageInventorySlots().forEach(s -> {
 						if (s.getItem().isEmpty()) {
-							targets.add(new GhostTarget<>(screen, ghostStack, s));
+							targets.add(new ReiGhostTarget() {
+								@Override
+								public VoxelShape bounds() {
+									return DraggableBoundsProvider.fromRectangle(new Rectangle(screen.getGuiLeft() + s.x, screen.getGuiTop() + s.y, 17, 17));
+								}
+
+								@Override
+								public void accept() {
+									PacketDistributor.sendToServer(new SetMemorySlotPayload(ghostStack, s.index));
+								}
+							});
 						}
 					});
 				}
@@ -81,24 +74,4 @@ public class ReiSettingsGhostIngredientHandler<S extends SettingsScreen> impleme
 		return this.handingScreenClass.isInstance(screen);
 	}
 
-	private static class GhostTarget<I, S extends SettingsScreen> implements BoundsProvider {
-		private final Rectangle area;
-		private final Slot slot;
-		private final ItemStack stack;
-
-		public GhostTarget(S screen, ItemStack stack, Slot slot) {
-			this.slot = slot;
-			this.stack = stack;
-			this.area = new Rectangle(screen.getGuiLeft() + slot.x, screen.getGuiTop() + slot.y, 16, 16);
-		}
-
-		public void accept(I ingredient) {
-			PacketDistributor.sendToServer(new SetMemorySlotPayload(stack, slot.index));
-		}
-
-		@Override
-		public VoxelShape bounds() {
-			return BoundsProvider.fromRectangle(area);
-		}
-	}
 }
--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/rei/ReiStorageGhostIngredientHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/rei/ReiStorageGhostIngredientHandler.java
@@ -2,14 +2,9 @@ package net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.rei;
 
 import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.client.gui.drag.DraggableStack;
-import me.shedaniel.rei.api.client.gui.drag.DraggableStackVisitor;
-import me.shedaniel.rei.api.client.gui.drag.DraggedAcceptorResult;
-import me.shedaniel.rei.api.client.gui.drag.DraggingContext;
+import me.shedaniel.rei.api.client.gui.drag.*;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
@@ -31,27 +26,15 @@ public class ReiStorageGhostIngredientHandler<S extends StorageScreenBase<?>> im
 
 	@Override
 	public DraggedAcceptorResult acceptDraggedStack(DraggingContext<S> context, DraggableStack stack) {
-		Stream<BoundsProvider> bounds = getDraggableAcceptingBounds(context, stack);
-		Point cursor = context.getCurrentPosition();
+        Point cursor = context.getCurrentPosition();
 		if (cursor != null) {
-			int x = cursor.getX();
-			int y = cursor.getY();
-			Optional<BoundsProvider> target = bounds.filter(b -> {
-				AABB box = b.bounds().bounds();
-				double minX = box.minX;
-				double minY = box.minY;
-				double maxX = box.maxX;
-				double maxY = box.maxY;
-				return x >= minX && x <= maxX && y >= minY && y <= maxY && b instanceof GhostTarget;
-			}).findFirst();
+            Optional<ReiGhostTarget> target = getDraggableAcceptingBounds(context, stack)
+					.map(ReiGhostTarget.class::cast)
+					.filter(b -> b.contains(cursor.getX(), cursor.getY()))
+					.findFirst();
 			if (target.isPresent()) {
-				//noinspection unchecked
-				GhostTarget<ItemStack, ? extends StorageScreenBase<?>> ghost = (GhostTarget<ItemStack, ? extends StorageScreenBase<?>>) target.get();
-				Object held = stack.getStack().getValue();
-				if (held instanceof ItemStack item) {
-					ghost.accept(item);
-					return DraggedAcceptorResult.CONSUMED;
-				}
+				target.get().accept();
+				return DraggedAcceptorResult.CONSUMED;
 			}
 		}
 		return DraggableStackVisitor.super.acceptDraggedStack(context, stack);
@@ -60,12 +43,23 @@ public class ReiStorageGhostIngredientHandler<S extends StorageScreenBase<?>> im
 	@Override
 	public Stream<BoundsProvider> getDraggableAcceptingBounds(DraggingContext<S> context, DraggableStack stack) {
 		List<BoundsProvider> targets = new ArrayList<>();
-		StorageContainerMenuBase<?> screen = context.getScreen().getMenu();
+		StorageScreenBase<?> screen = context.getScreen();
+		StorageContainerMenuBase<?> menu = context.getScreen().getMenu();
 
 		if (stack.getStack().getValue() instanceof ItemStack ghostStack) {
-			screen.getOpenContainer().ifPresent(c -> c.getSlots().forEach(s -> {
+			menu.getOpenContainer().ifPresent(c -> c.getSlots().forEach(s -> {
 				if (s instanceof IFilterSlot && s.mayPlace(ghostStack)) {
-					targets.add(new GhostTarget<>(context.getScreen(), ghostStack, s));
+					targets.add(new ReiGhostTarget() {
+						@Override
+						public VoxelShape bounds() {
+							return DraggableBoundsProvider.fromRectangle(new Rectangle(screen.getGuiLeft() + s.x, screen.getGuiTop() + s.y, 17, 17));
+						}
+
+						@Override
+						public void accept() {
+							PacketDistributor.sendToServer(new SetGhostSlotPayload(ghostStack, s.index));
+						}
+					});
 				}
 			}));
 		}
@@ -76,26 +70,5 @@ public class ReiStorageGhostIngredientHandler<S extends StorageScreenBase<?>> im
 	@Override
 	public <R extends Screen> boolean isHandingScreen(R screen) {
 		return this.handingScreenClass.isInstance(screen);
-	}
-
-	private static class GhostTarget<I, S extends StorageScreenBase<?>> implements BoundsProvider {
-		private final Rectangle area;
-		private final Slot slot;
-		private final ItemStack stack;
-
-		public GhostTarget(S screen, ItemStack stack, Slot slot) {
-			this.slot = slot;
-			this.stack = stack;
-			this.area = new Rectangle(screen.getGuiLeft() + slot.x, screen.getGuiTop() + slot.y, 16, 16);
-		}
-
-		public void accept(I ingredient) {
-			PacketDistributor.sendToServer(new SetGhostSlotPayload(stack, slot.index));
-		}
-
-		@Override
-		public VoxelShape bounds() {
-			return BoundsProvider.fromRectangle(area);
-		}
 	}
 }
