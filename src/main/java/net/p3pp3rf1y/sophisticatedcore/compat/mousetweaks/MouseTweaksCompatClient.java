d42242f6ce1832205829fdc3a0d7d49f9c2962c4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-06-21 20:54:09 +0200

    fix: 🐛 Fixed mouse tweaks scroll compatibility so now scrolling of items out / into the scrollable storage inventory works again

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/mousetweaks/MouseTweaksCompatClient.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/mousetweaks/MouseTweaksCompatClient.java
@@ -4,6 +4,6 @@ import net.p3pp3rf1y.sophisticatedcore.client.gui.controls.InventoryScrollPanel;
 
 public class MouseTweaksCompatClient {
 	public static void restrictSophisticatedScrollInteraction() {
-		InventoryScrollPanel.setRestrictScrollToScrollbar(false);
+		InventoryScrollPanel.setRestrictScrollToScrollbar(true);
 	}
 }
