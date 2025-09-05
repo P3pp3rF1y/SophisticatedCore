d6f177423786b80c5c2258babf884cc07d8a91e0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-09-05 11:43:56 +0200

    feat: ✨ Added client config "mouseTweaksScrollEnabled" that allows to switch off the Mouse Tweaks compatibility that lets Mouse Tweaks handle the mouse scrolling of items in and out of player's inventory. It is on by default but when switched off in scrollable Sophisticated inventories mouse scroll will just scroll through slots in the inventory as it does when Mouse Tweaks isn't added in the modpack.

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/mousetweaks/MouseTweaksCompatClient.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/mousetweaks/MouseTweaksCompatClient.java
@@ -1,9 +1,12 @@
 package net.p3pp3rf1y.sophisticatedcore.compat.mousetweaks;
 
+import net.p3pp3rf1y.sophisticatedcore.Config;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.controls.InventoryScrollPanel;
 
 public class MouseTweaksCompatClient {
 	public static void restrictSophisticatedScrollInteraction() {
-		InventoryScrollPanel.setRestrictScrollToScrollbar(true);
+		if (Config.CLIENT.mouseTweaksScrollEnabled.get()) {
+			InventoryScrollPanel.setRestrictScrollToScrollbar(true);
+		}
 	}
 }
