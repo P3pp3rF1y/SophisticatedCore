64d8a923639dda6e4b5d153c0770c71f2d961985
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-10-01 16:06:27 +0200

    feat: ✨ Added sounds to search item functionality and tuned sounds of deposit from player's inventory

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/network/RequestItemHighlightsPayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/network/RequestItemHighlightsPayload.java
@@ -9,6 +9,8 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -20,6 +22,7 @@ import net.p3pp3rf1y.sophisticatedcore.common.ItemActionHandlerRegistry;
 import net.p3pp3rf1y.sophisticatedcore.controller.IControllableStorage;
 import net.p3pp3rf1y.sophisticatedcore.inventory.ISlotTracker;
 import net.p3pp3rf1y.sophisticatedcore.inventory.ItemStackKey;
+import net.p3pp3rf1y.sophisticatedcore.util.RandHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
 
 import java.util.ArrayList;
@@ -77,6 +80,7 @@ public record RequestItemHighlightsPayload(ItemStack stack,
 			Component message = null;
 			if (stackMatchNumber.get() == 0 && itemMatchNumber.get() == 0) {
 				message = Component.translatable("gui.sophisticatedcore.status.no_matching_items_found");
+				player.playNotifySound(SoundEvents.NOTE_BLOCK_BASS.value(), SoundSource.PLAYERS, 1, 0.7f + RandHelper.getRandomMinusOneToOne(level.random) * 0.1F);
 			} else {
 				if (stackMatchNumber.get() > 0) {
 					message = Component.translatable("gui.sophisticatedcore.status.matching_stacks_found", Component.literal(String.valueOf(stackMatchNumber.get())).withColor(0x4CAF50));
@@ -89,6 +93,7 @@ public record RequestItemHighlightsPayload(ItemStack stack,
 						message = itemMessage;
 					}
 				}
+				player.playNotifySound(SoundEvents.NOTE_BLOCK_CHIME.value(), SoundSource.PLAYERS, 1, 0.95f + RandHelper.getRandomMinusOneToOne(level.random) * 0.1F);
 			}
 
 			player.displayClientMessage(message, true);
