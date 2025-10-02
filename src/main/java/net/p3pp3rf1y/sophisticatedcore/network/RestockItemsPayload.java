a68d6e4298268475046815fc1ddc95b0122b3151
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-10-02 17:27:32 +0200

    feat: ✨ Tuned position of where restocked stacks aim when flying toward player and updated number of restocked items to show number of stacks changed in player's inventory.

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/network/RestockItemsPayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/network/RestockItemsPayload.java
@@ -70,47 +70,48 @@ public record RestockItemsPayload(ItemStack filter, int minSlot, int maxSlot, bo
 		});
 
 		ItemStackKey filterStackKey = ItemStackKey.of(payload.filter());
-		Map<Vec3, ItemStack> restocked = new HashMap<>();
+		Map<Vec3, ItemStack> transferredItems = new HashMap<>();
+		Set<Integer> restockedPlayerSlots = new HashSet<>();
 		for (int playerInventorySlot = payload.minSlot(); playerInventorySlot < payload.maxSlot(); playerInventorySlot++) {
 			ItemStack playerInventoryStack = player.getInventory().getItem(playerInventorySlot);
 			if (!payload.filter().isEmpty()) {
 				if (playerInventoryStack.isEmpty() || ItemStack.isSameItemSameComponents(playerInventoryStack, payload.filter())) {
-					restockSlot(restockHandlers, filterStackKey, playerInventoryStack, restocked, player, playerInventorySlot);
+					restockSlot(restockHandlers, filterStackKey, playerInventoryStack, transferredItems, restockedPlayerSlots, player, playerInventorySlot);
 				}
 			} else {
 				if (!playerInventoryStack.isEmpty()) {
-					restockSlot(restockHandlers, ItemStackKey.of(playerInventoryStack), playerInventoryStack, restocked, player, playerInventorySlot);
+					restockSlot(restockHandlers, ItemStackKey.of(playerInventoryStack), playerInventoryStack, transferredItems, restockedPlayerSlots, player, playerInventorySlot);
 				}
 			}
 		}
 
 		if (player instanceof ServerPlayer serverPlayer) {
-			Vec3 playerPos = player.getEyePosition().add(0, -0.1, 0);
-			PacketDistributor.sendToPlayer(serverPlayer, new SyncItemTransfersPayload(restocked, playerPos, false));
-			PacketDistributor.sendToPlayersTrackingEntity(serverPlayer, new SyncItemTransfersPayload(restocked, playerPos, false));
+			Vec3 playerPos = player.getEyePosition().add(0, -0.3, 0);
+			PacketDistributor.sendToPlayer(serverPlayer, new SyncItemTransfersPayload(transferredItems, playerPos, false));
+			PacketDistributor.sendToPlayersTrackingEntity(serverPlayer, new SyncItemTransfersPayload(transferredItems, playerPos, false));
 		}
 		Component message;
 		if (payload.maxSlot() - payload.minSlot() == 1) {
-			if (restocked.isEmpty()) {
+			if (transferredItems.isEmpty()) {
 				message = TranslationHelper.INSTANCE.translStatusMessage("cannot_restock_item",
 						Component.literal(player.getInventory().getItem(payload.minSlot()).getHoverName().getString()).withStyle(ChatFormatting.RED));
 				player.playNotifySound(SoundEvents.NOTE_BLOCK_BASS.value(), SoundSource.PLAYERS, 1, 0.7f + RandHelper.getRandomMinusOneToOne(level.random) * 0.1F);
 			} else {
 				message = TranslationHelper.INSTANCE.translStatusMessage("restocked_item",
-						Component.literal(restocked.values().iterator().next().getHoverName().getString()).withStyle(ChatFormatting.DARK_GREEN));
+						Component.literal(transferredItems.values().iterator().next().getHoverName().getString()).withStyle(ChatFormatting.DARK_GREEN));
 			}
 		} else {
-			if (restocked.isEmpty()) {
+			if (transferredItems.isEmpty()) {
 				message = TranslationHelper.INSTANCE.translStatusMessage("cannot_restock_items");
 				player.playNotifySound(SoundEvents.NOTE_BLOCK_BASS.value(), SoundSource.PLAYERS, 1, 0.7f + RandHelper.getRandomMinusOneToOne(level.random) * 0.1F);
 			} else {
-				message = TranslationHelper.INSTANCE.translStatusMessage("restocked_items", Component.literal(String.valueOf(restocked.size())).withStyle(ChatFormatting.DARK_GREEN));
+				message = TranslationHelper.INSTANCE.translStatusMessage("restocked_items", Component.literal(String.valueOf(restockedPlayerSlots.size())).withStyle(ChatFormatting.DARK_GREEN));
 			}
 		}
 		player.displayClientMessage(message, true);
 	}
 
-	private static void restockSlot(Map<Vec3, InventoryHandler> restockHandlers, ItemStackKey stackKey, ItemStack playerInventoryStack, Map<Vec3, ItemStack> restocked, Player player, int playerInventorySlot) {
+	private static void restockSlot(Map<Vec3, InventoryHandler> restockHandlers, ItemStackKey stackKey, ItemStack playerInventoryStack, Map<Vec3, ItemStack> restocked, Set<Integer> restockedPlayerSlots, Player player, int playerInventorySlot) {
 		for (Map.Entry<Vec3, InventoryHandler> entry : restockHandlers.entrySet()) {
 			Vec3 pos = entry.getKey();
 			InventoryHandler handler = entry.getValue();
@@ -125,6 +126,7 @@ public record RestockItemsPayload(ItemStack filter, int minSlot, int maxSlot, bo
 						playerInventoryStack.grow(extracted.getCount());
 					}
 					player.getInventory().setItem(playerInventorySlot, playerInventoryStack);
+					restockedPlayerSlots.add(playerInventorySlot);
 				}
 				matchingStackSlot = handler.getSlotTracker().getFirstMatchingSlot(stackKey);
 			}
