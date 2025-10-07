b9d56dadbf46388b13c56400b1a515c07ac61b05
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-10-07 10:31:08 +0200

    feat: ✨ Added support for FTB Chunk claims when checking whether restock / deposit may interact with storages

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/network/DepositItemsPayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/network/DepositItemsPayload.java
@@ -67,14 +67,14 @@ public record DepositItemsPayload(int minSlot, int maxSlot,
 
 		if (player.level() instanceof ServerLevel serverLevel) {
 			payload.storagePositions().stream()
-					.filter(pos -> player.mayInteract(serverLevel, pos))
+					.filter(pos -> WorldHelper.playerMayInteract(player, pos))
 					.map(pos -> WorldHelper.getBlockEntity(level, pos, IControllableStorage.class))
 					.filter(Optional::isPresent).map(Optional::get)
 					.forEach(s -> s.getControllerPos().ifPresentOrElse(controllerPositions::add,
 							() -> depositHandlers.put(s.getStorageBlockPos().getCenter(), new StorageDepositHandler(s.getStorageWrapper().getInventoryHandler()))));
 
 			controllerPositions.stream()
-					.filter(pos -> player.mayInteract(serverLevel, pos))
+					.filter(pos -> WorldHelper.playerMayInteract(player, pos))
 					.map(pos -> WorldHelper.getBlockEntity(player.level(), pos, ControllerBlockEntityBase.class))
 					.filter(Optional::isPresent).map(Optional::get)
 					.forEach(c -> depositHandlers.put(c.getBlockPos().getCenter(), new ControllerDepositHandler(c)));
