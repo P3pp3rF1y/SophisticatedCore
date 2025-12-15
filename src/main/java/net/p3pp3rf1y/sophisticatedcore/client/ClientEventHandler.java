eb07c256da648ada46c4a9be93f744d47a402b99
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-12-15 22:25:23 +0100

    fix: 🐛 Fixed restock to player's inventory functionality to not trigger when pressing its key while in sign text edit screen. Also made it only trigger while in game or in any while hovering over non empty slot.

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/client/ClientEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/client/ClientEventHandler.java
@@ -106,7 +106,8 @@ public class ClientEventHandler {
 	}
 
 	public static void handleKeyInput(InputEvent.Key event) {
-		if (Minecraft.getInstance().screen != null && Minecraft.getInstance().screen.isFocused()) {
+		Screen screen = Minecraft.getInstance().screen;
+		if (screen != null && screen.isFocused()) {
 			return;
 		}
 
@@ -142,14 +143,17 @@ public class ClientEventHandler {
 		ItemStack filter = getHoveredStack();
 		int slot;
 		if (filter.isEmpty()) {
-			if (screen instanceof AbstractContainerScreen<?> containerScreen) {
-				Slot slotUnderMouse = containerScreen.getSlotUnderMouse();
-				if (slotUnderMouse != null) {
-					filter = slotUnderMouse.getItem();
-					slot = slotUnderMouse.getSlotIndex();
+			if (screen != null) {
+				if (screen instanceof AbstractContainerScreen<?> containerScreen) {
+					Slot slotUnderMouse = containerScreen.getSlotUnderMouse();
+					if (slotUnderMouse != null && !slotUnderMouse.getItem().isEmpty()) {
+						filter = slotUnderMouse.getItem();
+						slot = slotUnderMouse.getSlotIndex();
+					} else {
+						return; // exit if not hovering over a slot
+					}
 				} else {
-					filter = ItemStack.EMPTY;
-					slot = player.getInventory().selected;
+					return; // exit if not in a container screen
 				}
 			} else {
 				filter = player.getMainHandItem();
