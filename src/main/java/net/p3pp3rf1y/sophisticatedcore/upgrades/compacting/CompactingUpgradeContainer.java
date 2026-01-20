ae284867d4fff3a5103989a1455e6c755749b666
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-20 10:39:54 +0100

    fix: 🐛 Fixed Compacting Upgrade not resuming compacting when the result had no space and the input slot filled up

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/compacting/CompactingUpgradeContainer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/compacting/CompactingUpgradeContainer.java
@@ -37,8 +37,8 @@ public class CompactingUpgradeContainer extends UpgradeContainerBase<CompactingU
 			setCompactNonUncraftable(data.getBoolean(DATA_SHOULD_COMPACT_NON_UNCRAFTABLE));
 		} else if (data.contains(DATA_SHOULD_WORKD_IN_GUI)) {
 			setShouldWorkdInGUI(data.getBoolean(DATA_SHOULD_WORKD_IN_GUI));
-		} else {
-			filterLogicContainer.handlePacket(data);
+		} else if (filterLogicContainer.handlePacket(data)) {
+			upgradeWrapper.resetFullSlotInfo();
 		}
 	}
 
