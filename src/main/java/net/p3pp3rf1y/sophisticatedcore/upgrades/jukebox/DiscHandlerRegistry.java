fd42513ac0e063613d93f94ee775add1d326639d
IMG <1973581872@qq.com>
2026-02-02 04:39:18 +0800

    feat: ✨ Allow custom discs to generate in mob backpacks

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/jukebox/DiscHandlerRegistry.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/jukebox/DiscHandlerRegistry.java
@@ -1,12 +1,15 @@
 package net.p3pp3rf1y.sophisticatedcore.upgrades.jukebox;
 
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.p3pp3rf1y.sophisticatedcore.api.IDiscHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DiscHandlerRegistry {
 
@@ -14,6 +17,10 @@ public class DiscHandlerRegistry {
 
     static {
         registerHandler(new VanillaDiscHandler());
+    }
+
+    public static List<IDiscHandler<?>> getHandlers() {
+        return HANDLERS;
     }
 
     public static void registerHandler(IDiscHandler<?> handler) {
@@ -35,5 +42,24 @@ public class DiscHandlerRegistry {
 
     public static boolean isSupported(ItemStack itemStack) {
         return findHandler(itemStack).isPresent();
+    }
+
+    public static Optional<ItemStack> getRandomDisc(RandomSource rnd) {
+        Map<IDiscHandler<?>, Integer> handlerWeightMap = HANDLERS.stream()
+                .collect(Collectors.toMap(handler -> handler, IDiscHandler::getMusicDiscSize));
+        int totalWeight = handlerWeightMap.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        int randomWeight = rnd.nextInt(totalWeight);
+
+        int cumulativeWeight = 0;
+        for (Map.Entry<IDiscHandler<?>, Integer> entry : handlerWeightMap.entrySet()) {
+            cumulativeWeight += entry.getValue();
+            if (randomWeight < cumulativeWeight) {
+                return entry.getKey().getRandomDisc(rnd);
+            }
+        }
+        return Optional.empty();
     }
 }
