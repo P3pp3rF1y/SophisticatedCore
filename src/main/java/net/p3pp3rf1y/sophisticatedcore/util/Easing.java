be24e81e9dc224f2efc45fa877089ce7015cfe53
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-02-15 19:36:41 +0100

    feat: ✨ Added a bit of support for animations in Item Actions mod

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/util/Easing.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/util/Easing.java
@@ -1,10 +1,22 @@
 package net.p3pp3rf1y.sophisticatedcore.util;
 
+import net.minecraft.util.Mth;
+
 import java.util.function.Function;
 
 public class Easing {
 	public static final Easing EASE_IN_OUT_CUBIC = new Easing(number -> (float) (number < 0.5 ? 4 * number * number * number : (1 - Math.pow(-2 * number + 2, 3) / 2)));
 	public static final Easing EASE_OUT_CUBIC = new Easing(number -> (float) (1f - Math.pow(1 - number, 3)));
+	public static final Easing EASE_IN_OUT_QUINT = new Easing(number -> (float) (number < 0.5 ? 16 * Math.pow(number, 5) : (1 - Math.pow(-2 * number + 2, 5) / 2)));
+	public static final Easing EASE_IN_CUBIC_OUT_QUINT = new Easing(number -> {
+		number = Mth.clamp(number, 0f, 1f);
+
+		if (number < 0.5f) {
+			return (float) (0.5f * Math.pow(number * 2f, 3));
+		} else {
+			return (float) (1f - 0.5f * (1f - (1 - Math.pow(1 - ((number - 0.5f) * 2f), 5))));
+		}
+	});
 
 	private final Function<Float, Float> easingFunction;
 
