6b8e87fe58b1a4b8a7a32b90e2ac429c973a7787
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-03 23:29:38 +0200

    chore: 🔧 Port mounted Create storage cleanup hardening

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/MixinAbstractContraptionEntity.java
@@ -0,0 +1,31 @@
+package net.p3pp3rf1y.sophisticatedcore.mixin;
+
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import com.simibubi.create.content.contraptions.Contraption;
+import net.minecraft.world.entity.Entity;
+import net.p3pp3rf1y.sophisticatedcore.compat.create.MountedStorageBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractContraptionEntity.class)
+public class MixinAbstractContraptionEntity {
+	@Shadow
+	protected Contraption contraption;
+
+	@Inject(method = "remove", at = @At("HEAD"))
+	private void cleanupMountedStoragesOnDestroy(Entity.RemovalReason removalReason, CallbackInfo ci) {
+		AbstractContraptionEntity contraptionEntity = (AbstractContraptionEntity) (Object) this;
+		if (contraptionEntity.level().isClientSide() || contraption == null || contraption.disassembled || !removalReason.shouldDestroy()) {
+			return;
+		}
+
+		contraption.getStorage().getAllItemStorages().values().forEach(storage -> {
+			if (storage instanceof MountedStorageBase mountedStorage) {
+				mountedStorage.onContraptionDestroyed();
+			}
+		});
+	}
+}
