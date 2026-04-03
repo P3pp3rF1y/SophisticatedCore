63066d6aa8f5c26079a5b409d7791b58a5c0f7e6
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-03 23:29:39 +0200

    chore: 🔧 Port mounted Create storage cleanup hardening

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/create/MountedStorageBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/create/MountedStorageBase.java
@@ -114,4 +114,8 @@ public abstract class MountedStorageBase extends MountedItemStorage implements S
 	protected void onOpen(ServerLevel level, Vec3 pos) {
 
 	}
+
+	public void onContraptionDestroyed() {
+
+	}
 }
