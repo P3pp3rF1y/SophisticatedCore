96b087d0bba6ebe8281155bd5f69853b28c1434c
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-06 19:55:12 +0200

    chore: 🔧 Port EMI settings screen fix

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/emi/EmiCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/emi/EmiCompat.java
@@ -1,15 +1,19 @@
 package net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.emi;
 
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.inventory.Slot;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.p3pp3rf1y.sophisticatedcore.SophisticatedCore;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.SettingsContainerMenu;
 import net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.common.CommonPayloads;
 
 public class EmiCompat implements ICompat {
 	@Override
 	public void init(IEventBus modBus) {
+		SettingsContainerMenu.setConstructCallback(menu -> menu.addInternalCompatibilitySlot(new EmiAnchorSlot()));
 		modBus.addListener(this::registerPackets);
 	}
 
@@ -23,5 +27,16 @@ public class EmiCompat implements ICompat {
 		registrar.optional().playToServer(EmiTransferRecipePayload.TYPE, EmiTransferRecipePayload.STREAM_CODEC, EmiTransferRecipePayload::handlePayload);
 
 		CommonPayloads.registerPackets(registrar);
+	}
+
+	private static class EmiAnchorSlot extends Slot {
+		public EmiAnchorSlot() {
+			super(new SimpleContainer(1), 0, -10000, -10000);
+		}
+
+		@Override
+		public boolean isActive() {
+			return false;
+		}
 	}
 }
