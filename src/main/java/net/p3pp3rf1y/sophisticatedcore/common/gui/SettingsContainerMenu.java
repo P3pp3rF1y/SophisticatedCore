ac5a71adeb043192196c6c095e003113a65dc816
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-06 19:27:55 +0200

    fix: 🐛 Fixed EMI to show on settings screens

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/SettingsContainerMenu.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/SettingsContainerMenu.java
@@ -39,10 +39,12 @@ import net.p3pp3rf1y.sophisticatedcore.settings.nosort.NoSortSettingsContainer;
 import javax.annotation.Nullable;
 import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public abstract class SettingsContainerMenu<S extends IStorageWrapper> extends AbstractContainerMenu implements ISyncedContainer, IAdditionalSlotInfoMenu {
 	private static final Map<String, ISettingsContainerFactory<?, ?>> SETTINGS_CONTAINER_FACTORIES = new HashMap<>();
+	private static Consumer<SettingsContainerMenu<?>> constructCallback = menu -> {};
 
 	static {
 		addFactory(MainSettingsCategory.NAME, MainSettingsContainer::new);
@@ -70,10 +72,19 @@ public abstract class SettingsContainerMenu<S extends IStorageWrapper> extends A
 		super(menuType, windowId);
 		this.player = player;
 		this.storageWrapper = storageWrapper;
+		constructCallback.accept(this);
 
 		addStorageInventorySlots();
 		addSettingsContainers();
 		templatePersistanceContainer = new TemplatePersistanceContainer(this, player.level().registryAccess());
+	}
+
+	public static void setConstructCallback(@Nullable Consumer<SettingsContainerMenu<?>> constructCallback) {
+		SettingsContainerMenu.constructCallback = constructCallback == null ? menu -> {} : constructCallback;
+	}
+
+	public Slot addInternalCompatibilitySlot(Slot slot) {
+		return super.addSlot(slot);
 	}
 
 	public int getNumberOfStorageInventorySlots() {
