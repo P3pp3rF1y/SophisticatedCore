00ad62db0bff88879c47cf25b3e2ede5760a9c08
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-12 15:27:27 +0200

    Revert "feat: ✨ Added render data validation to prevent incorrect particles"
    This reverts commit 1453b9ebbcf10c88250e732f55a709926ec7bd0a.

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/api/IUpgradeRenderDataValidator.java
+++
@@ -1,8 +0,0 @@
-package net.p3pp3rf1y.sophisticatedcore.api;
-
-import net.minecraft.world.level.Level;
-import net.p3pp3rf1y.sophisticatedcore.renderdata.IUpgradeClientData;
-
-public interface IUpgradeRenderDataValidator<T extends IUpgradeClientData> {
-	boolean isValid(IStorageWrapper storageWrapper, Level level, T upgradeClientData);
-}
