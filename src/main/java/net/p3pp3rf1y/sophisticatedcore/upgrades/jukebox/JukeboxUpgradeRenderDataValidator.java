f95882a994fa352d59b509f2a21acc8772d98558
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-12 16:19:52 +0200

    feat: ✨ Added render data validation to prevent incorrect particles

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/jukebox/JukeboxUpgradeRenderDataValidator.java
@@ -0,0 +1,12 @@
+package net.p3pp3rf1y.sophisticatedcore.upgrades.jukebox;
+
+import net.minecraft.world.level.Level;
+import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
+import net.p3pp3rf1y.sophisticatedcore.api.IUpgradeRenderDataValidator;
+
+public class JukeboxUpgradeRenderDataValidator implements IUpgradeRenderDataValidator<JukeboxUpgradeClientData> {
+	@Override
+	public boolean isValid(IStorageWrapper storageWrapper, Level level, JukeboxUpgradeClientData upgradeClientData) {
+		return upgradeClientData.isPlaying() && storageWrapper.getUpgradeHandler().getTypeWrappers(JukeboxUpgradeItem.TYPE).stream().anyMatch(JukeboxUpgradeWrapper::isPlaying);
+	}
+}
