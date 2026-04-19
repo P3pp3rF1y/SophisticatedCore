239ab5f28b3a43d6848bfb22dcb0a327470c5e76
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-20 00:06:07 +0200

    feat: ✨ Added in-game NeoForge config GUI support and labels

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/SortButtonsPosition.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/SortButtonsPosition.java
@@ -1,8 +1,19 @@
 package net.p3pp3rf1y.sophisticatedcore.client.gui;
 
-public enum SortButtonsPosition {
+import net.minecraft.network.chat.Component;
+import net.neoforged.neoforge.common.TranslatableEnum;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.TranslationHelper;
+
+import java.util.Locale;
+
+public enum SortButtonsPosition implements TranslatableEnum {
 	TITLE_LINE_RIGHT,
 	BELOW_UPGRADES,
 	BELOW_UPGRADE_TABS,
-	HIDDEN
+	HIDDEN;
+
+	@Override
+	public Component getTranslatedName() {
+		return Component.translatable(TranslationHelper.INSTANCE.translConfig("sortButtonsPosition." + name().toLowerCase(Locale.ROOT)));
+	}
 }
