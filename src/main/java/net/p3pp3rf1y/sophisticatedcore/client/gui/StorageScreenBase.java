c39b8e3b902eb5160159d318df709df594ba5504
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-03 23:13:09 +0200

    feat: ✨ Added refill functionality to stonecutter, sawmill and chipped upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/StorageScreenBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/StorageScreenBase.java
@@ -573,8 +573,6 @@ public abstract class StorageScreenBase<S extends StorageContainerMenuBase<?>> e
 		if (searchBox != null) {
 			searchBox.render(guiGraphics, mouseX, mouseY, partialTicks);
 		}
-
-		settingsTabControl.renderTooltip(this, guiGraphics, mouseX, mouseY);
 		if (sortButton != null && sortByButton != null) {
 			sortButton.render(guiGraphics, mouseX, mouseY, partialTicks);
 			sortByButton.render(guiGraphics, mouseX, mouseY, partialTicks);
@@ -582,6 +580,7 @@ public abstract class StorageScreenBase<S extends StorageContainerMenuBase<?>> e
 		upgradeSwitches.forEach(us -> us.render(guiGraphics, mouseX, mouseY, partialTicks));
 		renderErrorOverlay(guiGraphics);
 		if (modalOverlay == null) {
+			settingsTabControl.renderTooltip(this, guiGraphics, mouseX, mouseY);
 			renderTooltip(guiGraphics, mouseX, mouseY);
 		} else {
 			renderModalOverlay(guiGraphics, mouseX, mouseY, partialTicks);
