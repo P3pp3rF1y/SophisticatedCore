98c0bc9175d75dab0fa9f1df36128188690403fe
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-02 12:46:53 +0200

    fix: 🐛 Fixed jukebox music stopping in singleplayer

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/inventory/StorageWrapperRepository.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/inventory/StorageWrapperRepository.java
@@ -8,6 +8,7 @@ import net.p3pp3rf1y.sophisticatedcore.SophisticatedCore;
 import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -16,12 +17,12 @@ import java.util.function.Function;
 
 public class StorageWrapperRepository {
 
-	private static final Cache<ItemStack, IStorageWrapper> stackStorageWrappers = CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build();
-	private static final Cache<UUID, IStorageWrapper> uuidStorageWrappers = CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build();
-	private static final Cache<UUID, Set<IStorageWrapper>> storageWrappersByUuid = CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build();
+	private static final Map<ThreadGroup, Cache<ItemStack, IStorageWrapper>> stackStorageWrappers = new MapMaker().weakKeys().makeMap();
+	private static final Map<ThreadGroup, Cache<UUID, IStorageWrapper>> uuidStorageWrappers = new MapMaker().weakKeys().makeMap();
+	private static final Map<ThreadGroup, Cache<UUID, Set<IStorageWrapper>>> storageWrappersByUuid = new MapMaker().weakKeys().makeMap();
 
 	public static <T extends IStorageWrapper> Optional<T> getExistingStorageWrapper(ItemStack stack, Class<T> wrapperClass) {
-		IStorageWrapper storageWrapper = stackStorageWrappers.getIfPresent(stack);
+		IStorageWrapper storageWrapper = getStackStorageWrappers().getIfPresent(stack);
 		if (wrapperClass.isInstance(storageWrapper)) {
 			return Optional.of(wrapperClass.cast(storageWrapper));
 		}
@@ -29,29 +30,30 @@ public class StorageWrapperRepository {
 	}
 
 	public static <T extends IStorageWrapper> T getStorageWrapper(ItemStack stack, Class<T> wrapperClass, Function<ItemStack, T> factory) {
-		IStorageWrapper storageWrapper = stackStorageWrappers.getIfPresent(stack);
+		Cache<ItemStack, IStorageWrapper> wrappers = getStackStorageWrappers();
+		IStorageWrapper storageWrapper = wrappers.getIfPresent(stack);
 		if (storageWrapper == null) {
 			storageWrapper = instantiateWrapper(stack, factory);
-			stackStorageWrappers.put(stack, storageWrapper);
+			wrappers.put(stack, storageWrapper);
 		} else if (!wrapperClass.isInstance(storageWrapper)) {
 			SophisticatedCore.LOGGER.error("StorageWrapperRepository: Wrapper with ItemStack {} is not an instance of {}. Replacing with new instance...", stack, wrapperClass);
-			stackStorageWrappers.invalidate(stack);
+			wrappers.invalidate(stack);
 			storageWrapper = instantiateWrapper(stack, factory);
-			stackStorageWrappers.put(stack, storageWrapper);
+			wrappers.put(stack, storageWrapper);
 		}
 		return wrapperClass.cast(storageWrapper);
 	}
 
 	public static void setStorageWrapper(ItemStack stack, IStorageWrapper storageWrapper) {
-		stackStorageWrappers.put(stack, storageWrapper);
+		getStackStorageWrappers().put(stack, storageWrapper);
 	}
 
 	public static void registerStorageWrapper(UUID storageUuid, IStorageWrapper storageWrapper) {
-		storageWrappersByUuid.asMap().computeIfAbsent(storageUuid, uuid -> Collections.newSetFromMap(new MapMaker().weakKeys().makeMap())).add(storageWrapper);
+		getStorageWrappersByUuid().asMap().computeIfAbsent(storageUuid, uuid -> Collections.newSetFromMap(new MapMaker().weakKeys().makeMap())).add(storageWrapper);
 	}
 
 	public static void invalidateStorageWrapperContents(UUID storageUuid, IStorageWrapper sourceWrapper) {
-		Set<IStorageWrapper> storageWrappers = storageWrappersByUuid.getIfPresent(storageUuid);
+		Set<IStorageWrapper> storageWrappers = getStorageWrappersByUuid().getIfPresent(storageUuid);
 		if (storageWrappers == null) {
 			return;
 		}
@@ -82,14 +84,29 @@ public class StorageWrapperRepository {
 	}
 
 	public static void migrateToUuid(IStorageWrapper storageWrapper, ItemStack stack, UUID storageUuid) {
-		stackStorageWrappers.invalidate(stack);
-		uuidStorageWrappers.put(storageUuid, storageWrapper);
+		getStackStorageWrappers().invalidate(stack);
+		getUuidStorageWrappers().put(storageUuid, storageWrapper);
 		registerStorageWrapper(storageUuid, storageWrapper);
 	}
 
 	public static void clearCache() {
-		stackStorageWrappers.invalidateAll();
-		uuidStorageWrappers.invalidateAll();
-		storageWrappersByUuid.invalidateAll();
+		stackStorageWrappers.values().forEach(Cache::invalidateAll);
+		uuidStorageWrappers.values().forEach(Cache::invalidateAll);
+		storageWrappersByUuid.values().forEach(Cache::invalidateAll);
+		stackStorageWrappers.clear();
+		uuidStorageWrappers.clear();
+		storageWrappersByUuid.clear();
+	}
+
+	private static Cache<ItemStack, IStorageWrapper> getStackStorageWrappers() {
+		return stackStorageWrappers.computeIfAbsent(Thread.currentThread().getThreadGroup(), threadGroup -> CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build());
+	}
+
+	private static Cache<UUID, IStorageWrapper> getUuidStorageWrappers() {
+		return uuidStorageWrappers.computeIfAbsent(Thread.currentThread().getThreadGroup(), threadGroup -> CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build());
+	}
+
+	private static Cache<UUID, Set<IStorageWrapper>> getStorageWrappersByUuid() {
+		return storageWrappersByUuid.computeIfAbsent(Thread.currentThread().getThreadGroup(), threadGroup -> CacheBuilder.newBuilder().expireAfterAccess(10L, TimeUnit.MINUTES).build());
 	}
 }
