05729c9f3f469f8516202508e00c6ad33459d227
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-09 21:01:47 +0200

    fix: 🐛 Fixed hidden memorized slots affecting backpack shift-click transfers

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/StorageContainerMenuBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/StorageContainerMenuBase.java
@@ -1478,7 +1478,7 @@ public abstract class StorageContainerMenuBase<S extends IStorageWrapper> extend
 			}
 		}
 
-		if (toTransfer > 0) {
+		if (toTransfer > 0 && endIndex <= getNumberOfStorageInventorySlots()) {
 			int firstIndex = reverseDirection ? endIndex - 1 : startIndex;
 			int increment = reverseDirection ? -1 : 1;
 
