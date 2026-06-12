377dd94529ccb7aedc13db379ad701aca23d2512
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-12 18:40:18 +0200

    feat: ✨ Added configurable compacting shapes

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/compacting/CompactingUpgradeItem.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/compacting/CompactingUpgradeItem.java
@@ -1,21 +1,29 @@
 package net.p3pp3rf1y.sophisticatedcore.upgrades.compacting;
 
+import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeCountLimitConfig;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeItemBase;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeType;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.IntSupplier;
 
 public class CompactingUpgradeItem extends UpgradeItemBase<CompactingUpgradeWrapper> {
 	private static final UpgradeType<CompactingUpgradeWrapper> TYPE = new UpgradeType<>(CompactingUpgradeWrapper::new);
 	private final boolean shouldCompactThreeByThree;
 	private final IntSupplier filterSlotCount;
+	private final ConfiguredCompactingResultProvider configuredCompactingResultProvider;
 
 	public CompactingUpgradeItem(boolean shouldCompactThreeByThree, IntSupplier filterSlotCount, IUpgradeCountLimitConfig upgradeTypeLimitConfig, Properties properties) {
+		this(shouldCompactThreeByThree, filterSlotCount, upgradeTypeLimitConfig, properties, (stack, maxWidth, maxHeight) -> Optional.empty());
+	}
+
+	public CompactingUpgradeItem(boolean shouldCompactThreeByThree, IntSupplier filterSlotCount, IUpgradeCountLimitConfig upgradeTypeLimitConfig, Properties properties, ConfiguredCompactingResultProvider configuredCompactingResultProvider) {
 		super(upgradeTypeLimitConfig, properties);
 		this.shouldCompactThreeByThree = shouldCompactThreeByThree;
 		this.filterSlotCount = filterSlotCount;
+		this.configuredCompactingResultProvider = configuredCompactingResultProvider;
 	}
 
 	@Override
@@ -34,5 +42,14 @@ public class CompactingUpgradeItem extends UpgradeItemBase<CompactingUpgradeWrap
 
 	public int getFilterSlotCount() {
 		return filterSlotCount.getAsInt();
+	}
+
+	public Optional<CompactingUpgradeConfig.CompactingDefinition> getConfiguredCompactingResult(ItemStack stack, int maxWidth, int maxHeight) {
+		return configuredCompactingResultProvider.getCompactingResult(stack, maxWidth, maxHeight);
+	}
+
+	@FunctionalInterface
+	public interface ConfiguredCompactingResultProvider {
+		Optional<CompactingUpgradeConfig.CompactingDefinition> getCompactingResult(ItemStack stack, int maxWidth, int maxHeight);
 	}
 }
