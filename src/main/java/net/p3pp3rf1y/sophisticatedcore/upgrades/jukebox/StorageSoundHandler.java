1323f3d4da24a8249baad002eefa5b0af509a9c9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-13 19:31:48 +0200

    feat: ✨ Added parrot and allay dancing to jukebox upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/jukebox/StorageSoundHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/jukebox/StorageSoundHandler.java
@@ -38,6 +38,16 @@ public class StorageSoundHandler {
 		}
 	}
 
+	public static boolean isStorageSoundPlayingNear(Vec3 position, double radius) {
+		double radiusSqr = radius * radius;
+		return storageSounds.values().stream().anyMatch(sound -> {
+			double xDiff = sound.getX() - position.x;
+			double yDiff = sound.getY() - position.y;
+			double zDiff = sound.getZ() - position.z;
+			return xDiff * xDiff + yDiff * yDiff + zDiff * zDiff <= radiusSqr;
+		});
+	}
+
 	public static void tick(LevelTickEvent.Post event) {
 		if (!storageSounds.isEmpty() && lastPlaybackChecked < event.getLevel().getGameTime() - SOUND_STOP_CHECK_INTERVAL) {
 			lastPlaybackChecked = event.getLevel().getGameTime();
