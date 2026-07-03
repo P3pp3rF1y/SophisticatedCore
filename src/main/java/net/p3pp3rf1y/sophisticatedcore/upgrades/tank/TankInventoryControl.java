4e1dbeab787f08951a4f73ac4ee04d48932c4019
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-03 08:55:39 +0200

    feat: ✨ Added support for rendering fluids lighter than air

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/tank/TankInventoryControl.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/upgrades/tank/TankInventoryControl.java
@@ -134,7 +134,8 @@ public class TankInventoryControl extends UpgradeInventoryControlBase {
 		IClientFluidTypeExtensions renderProperties = IClientFluidTypeExtensions.of(fluid);
 		ResourceLocation texture = renderProperties.getStillTexture(contents);
 		TextureAtlasSprite still = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(texture);
-		GuiHelper.renderTiledFluidTextureAtlas(guiGraphics, RenderType::guiTextured, still, renderProperties.getTintColor(contents), pos.x() + 10,
-				pos.y() + 1 + height - 2 - displayLevel, displayLevel);
+		int fluidY = fluid.getFluidType().isLighterThanAir() ? pos.y() + 1 : pos.y() + 1 + height - 2 - displayLevel;
+		GuiHelper.renderTiledFluidTextureAtlas(guiGraphics, RenderType::guiTextured, still, renderProperties.getTintColor(contents), pos.x() + 10, fluidY,
+				displayLevel);
 	}
 }
