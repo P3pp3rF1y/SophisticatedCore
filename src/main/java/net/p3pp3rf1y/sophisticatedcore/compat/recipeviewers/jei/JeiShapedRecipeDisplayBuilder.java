fe32800df65c52f3b5980c16aa745156c8460612
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-05 20:56:37 +0200

    fix: 🐛 Fixed JEI transfers into crafting upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/jei/JeiShapedRecipeDisplayBuilder.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/recipeviewers/jei/JeiShapedRecipeDisplayBuilder.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 public class JeiShapedRecipeDisplayBuilder extends ShapedRecipeDisplayBuilder<CraftingRecipe> {
 	private final JeiRecipeDisplayGenerator generator;
-	private final SlotDisplay result;
+	private final ItemStack result;
 	private final Map<Character, Ingredient> key = Maps.newLinkedHashMap();
 	private final Map<Character, SlotDisplay> displayKey = Maps.newLinkedHashMap();
 	private List<Optional<Ingredient>> ingredients = new ArrayList<>();
@@ -31,7 +31,7 @@ public class JeiShapedRecipeDisplayBuilder extends ShapedRecipeDisplayBuilder<Cr
 	public JeiShapedRecipeDisplayBuilder(HolderGetter<Item> items, JeiRecipeDisplayGenerator generator, ItemStack result) {
 		this.items = items;
 		this.generator = generator;
-		this.result = new SlotDisplay.ItemStackSlotDisplay(result);
+		this.result = result;
 	}
 
 	@Override
