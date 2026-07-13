d53e387de150e4f9e299194556879c3d96b16a52
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-13 09:45:55 +0200

    feat: ✨ Added item display depth controls for storages and backpacks

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/renderdata/RenderInfo.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/renderdata/RenderInfo.java
@@ -459,16 +459,19 @@ public abstract class RenderInfo {
 		private static final String ROTATION_TAG = "rotation";
 		private static final String SLOT_INDEX_TAG = "slotIndex";
 		private static final String DISPLAY_SIDE_TAG = "displaySide";
+		private static final String Z_OFFSET_TAG = "zOffset";
 		private final ItemStack item;
 		private final int rotation;
 		private final int slotIndex;
 		private final DisplaySide displaySide;
+		private final int zOffset;
 
-		public DisplayItem(ItemStack item, int rotation, int slotIndex, DisplaySide displaySide) {
+		public DisplayItem(ItemStack item, int rotation, int slotIndex, DisplaySide displaySide, int zOffset) {
 			this.item = item;
 			this.rotation = rotation;
 			this.slotIndex = slotIndex;
 			this.displaySide = displaySide;
+			this.zOffset = zOffset;
 		}
 
 		private CompoundTag serialize(CompoundTag tag) {
@@ -476,12 +479,14 @@ public abstract class RenderInfo {
 			tag.putInt(ROTATION_TAG, rotation);
 			tag.putInt(SLOT_INDEX_TAG, slotIndex);
 			tag.putString(DISPLAY_SIDE_TAG, displaySide.getSerializedName());
+			tag.putInt(Z_OFFSET_TAG, zOffset);
 			return tag;
 		}
 
 		private static DisplayItem deserialize(CompoundTag tag) {
 			return new DisplayItem(RegistryHelper.getRegistryAccess().map(registryAccess -> ItemStack.parseOptional(registryAccess, tag.getCompound(ITEM_TAG)))
-					.orElse(ItemStack.EMPTY), tag.getInt(ROTATION_TAG), tag.getInt(SLOT_INDEX_TAG), DisplaySide.fromName(tag.getString(DISPLAY_SIDE_TAG)));
+					.orElse(ItemStack.EMPTY), tag.getInt(ROTATION_TAG), tag.getInt(SLOT_INDEX_TAG), DisplaySide.fromName(tag.getString(DISPLAY_SIDE_TAG)),
+					tag.getInt(Z_OFFSET_TAG));
 		}
 
 		public ItemStack getItem() {
@@ -498,6 +503,10 @@ public abstract class RenderInfo {
 
 		public DisplaySide getDisplaySide() {
 			return displaySide;
+		}
+
+		public int getZOffset() {
+			return zOffset;
 		}
 	}
 }
