7607aeb7760c17a4f16db77fbe9156e68b2c27f9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-06 11:14:05 +0200

    feat: ✨ Added Stack Upgrade conversion items

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/StorageContainerMenuBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/gui/StorageContainerMenuBase.java
@@ -10,6 +10,8 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.HashedStack;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
@@ -41,6 +43,7 @@ import net.p3pp3rf1y.sophisticatedcore.settings.SettingsHandler;
 import net.p3pp3rf1y.sophisticatedcore.settings.main.MainSettingsCategoryData;
 import net.p3pp3rf1y.sophisticatedcore.settings.memory.MemorySettingsCategory;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.*;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.stack.StackUpgradeConversionItem;
 import net.p3pp3rf1y.sophisticatedcore.util.DummySlot;
 import net.p3pp3rf1y.sophisticatedcore.util.InventoryHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.MathHelper;
@@ -394,6 +397,24 @@ public abstract class StorageContainerMenuBase<S extends IStorageWrapper> extend
 			return;
 		} else if (isUpgradeSlot(slotId) && getSlot(slotId) instanceof StorageContainerMenuBase<?>.StorageUpgradeSlot slot) {
 			ItemStack slotStack = slot.getItem();
+			if (clickType == ClickType.PICKUP && dragType == 1 && getCarried().getItem() instanceof StackUpgradeConversionItem conversionItem) {
+				ItemStack targetUpgradeStack = conversionItem.getTargetUpgradeStack();
+				if (conversionItem.canConvert(slotStack) && slot.mayPlace(targetUpgradeStack)) {
+					slot.set(targetUpgradeStack);
+					if (!player.getAbilities().instabuild) {
+						getCarried().shrink(1);
+						if (getCarried().isEmpty()) {
+							setCarried(ItemStack.EMPTY);
+						}
+					}
+					slot.setChanged();
+					if (!player.level().isClientSide()) {
+						player.level().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SMITHING_TABLE_USE, SoundSource.PLAYERS, 0.8F,
+								1.0F);
+					}
+				}
+				return;
+			}
 			if (slot.mayPlace(getCarried())) {
 				ItemStack carriedStack = getCarried();
 				IUpgradeItem<?> upgradeItem = (IUpgradeItem<?>) carriedStack.getItem();
