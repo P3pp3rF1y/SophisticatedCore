4667af5b218d3afe61e2a67286aaa123958084b8
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-07 23:39:15 +0200

    fix: 🐛 Fixed hidden durability and NBT toggles receiving input

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/controls/WidgetBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/client/gui/controls/WidgetBase.java
@@ -82,7 +82,7 @@ public abstract class WidgetBase implements Renderable, GuiEventListener, Narrat
 
 	@Override
 	public boolean isMouseOver(double mouseX, double mouseY) {
-		return mouseX >= x && mouseX < x + getWidth() && mouseY >= y && mouseY < y + getHeight();
+		return visible && mouseX >= x && mouseX < x + getWidth() && mouseY >= y && mouseY < y + getHeight();
 	}
 
 	public int getX() {
