b66dea4cb83db9d7f3ca6fea5730e2662c8059b9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-09-08 00:55:06 +0200

    chore: 🔧 Port linked storage to SB Create integration

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/linkedstorage/ILinkedStorageBlockEndpoint.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/linkedstorage/ILinkedStorageBlockEndpoint.java
@@ -1,10 +1,26 @@
 package net.p3pp3rf1y.sophisticatedcore.linkedstorage;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+
 import javax.annotation.Nullable;
 
-public interface ILinkedStorageBlockEndpoint {
+import java.util.UUID;
+
+public interface ILinkedStorageBlockEndpoint extends ILinkedStorageInteractionTarget {
+	@Override
+	default boolean isLinkedStorageLinkCandidate() {
+		return true;
+	}
+
 	@Nullable
+	@Override
 	LinkedStorageEndpointData getLinkedStorageEndpointData();
 
 	ILinkedStorageEndpointAdapter<ILinkedStorageBlockEndpoint> getLinkedStorageBlockEndpointAdapter();
+
+	@Override
+	default LinkedStorageService.LinkResult link(ServerLevel level, UUID playerId, ItemStack linker) {
+		return LinkedStorageService.linkWithResult(level, playerId, linker, this);
+	}
 }
